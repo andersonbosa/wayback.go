70c0ada3773922c9513060bb29ed725a1c379868
andersonbosa <andersonbosa0@gmail.com>
2023-09-06 08:11:10 -0300

    remove deprecated `ioutils` module chore improve usage print

--- wayback.go/main.go
+++ wayback.go/main.go
@@ -3,36 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"sort"
 	"strings"
 )
 
-func searchWaybackMachine(target string, showDate bool) {
+// Result represents a Wayback Machine capture result.
+type Result struct {
+	Timestamp string
+	URL       string
+}
+
+func searchWaybackMachine(target string, showDetails bool) {
 	apiURL := fmt.Sprintf("http://web.archive.org/cdx/search/cdx?url=*.%s/*&output=txt&fl=original,timestamp", target)
 	response, err := http.Get(apiURL)
 
 	if err != nil {
-		fmt.Printf("Error accessing the Wayback Machine for target: %s\n", target)
+		fmt.Printf("Error accessing the Wayback Machine for target: %s - %v\n", target, err)
 		return
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode == 200 {
-		body, err := ioutil.ReadAll(response.Body)
+		body, err := io.ReadAll(response.Body)
 		if err != nil {
-			fmt.Printf("Error reading the Wayback Machine response for target: %s\n", target)
+			fmt.Printf("Error reading the Wayback Machine response for target: %s - %v\n", target, err)
 			return
 		}
 
 		results := strings.Split(string(body), "\n")
 		uniqueURLs := make(map[string]bool)
-		var dateURLList []struct {
-			Timestamp string
-			URL       string
-		}
+		var dateURLList []Result
 
 		if len(results) > 1 {
 			for _, result := range results[1:] {
@@ -41,10 +44,7 @@ func searchWaybackMachine(target string, showDate bool) {
 					url, timestamp := parts[0], parts[1]
 					if !uniqueURLs[url] {
 						uniqueURLs[url] = true
-						dateURLList = append(dateURLList, struct {
-							Timestamp string
-							URL       string
-						}{timestamp, url})
+						dateURLList = append(dateURLList, Result{timestamp, url})
 					}
 				}
 			}
@@ -55,7 +55,7 @@ func searchWaybackMachine(target string, showDate bool) {
 			})
 
 			for _, entry := range dateURLList {
-				if showDate {
+				if showDetails {
 					fmt.Println("---")
 					fmt.Printf("Target: %s\n", entry.URL)
 					fmt.Printf("Last updated %s: %s\n", target, entry.Timestamp)
@@ -68,17 +68,12 @@ func searchWaybackMachine(target string, showDate bool) {
 			fmt.Printf("No captures found for target: %s\n", target)
 		}
 	} else {
-		fmt.Printf("Error accessing the Wayback Machine for target: %s\n", target)
+		fmt.Printf("Error accessing the Wayback Machine for target: %s - Status Code: %d\n", target, response.StatusCode)
 	}
 }
 
-func main() {
-	targets := flag.String("targets", "", "Comma-separated list of targets to search (e.g., example.com,example2.com)")
-	showDate := flag.Bool("details", false, "Show details from Web Archive")
-	flag.Parse()
-
-	if *targets == "" {
-		fmt.Println(`
+func printUsage() {
+	fmt.Println(`
 _  _  _ _______ __   __ ______  _______ _______ _     _    ______  _____ 
 |  |  | |_____|   \_/   |_____] |_____| |       |____/    |  ____ |     |
 |__|__| |     |    |    |_____] |     | |_____  |    \_ . |_____| |_____|
@@ -87,11 +82,20 @@ _  _  _ _______ __   __ ______  _______ _______ _     _    ______  _____
 
 USAGE: wayback.go [-details] -targets target1,target2,target3
 `)
+}
+
+func main() {
+	targets := flag.String("targets", "", "Comma-separated list of targets to search (e.g., example.com,example2.com)")
+	showDetails := flag.Bool("details", false, "Show details from Web Archive")
+	flag.Parse()
+
+	if *targets == "" {
+		printUsage()
 		os.Exit(1)
 	}
 
 	targetsList := strings.Split(*targets, ",")
 	for _, target := range targetsList {
-		searchWaybackMachine(target, *showDate)
+		searchWaybackMachine(target, *showDetails)
 	}
 }
